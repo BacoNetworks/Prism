5c50d6af46f15fc6848ace9c1be0b4083bec5fc7
kristi71111 <kristjan.gorinek20@gmail.com>
2021-03-10 18:21:35 +0100

    Fix https://github.com/prism/Prism/issues/28 placement, breaking
    Also add licence back since I figured out out.

--- src/main/java/com/helion3/prism/listeners/EntityListener.java
+++ src/main/java/com/helion3/prism/listeners/EntityListener.java
@@ -28,11 +28,18 @@ import com.helion3.prism.Prism;
 import com.helion3.prism.api.records.PrismRecord;
 import com.helion3.prism.util.PrismEvents;
 import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.hanging.Hanging;
+import org.spongepowered.api.entity.hanging.ItemFrame;
+import org.spongepowered.api.entity.living.ArmorStand;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
+import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.command.SendCommandEvent;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
+import org.spongepowered.api.event.entity.InteractEntityEvent;
+import org.spongepowered.api.event.entity.SpawnEntityEvent;
+import org.spongepowered.api.event.entity.living.humanoid.HandInteractEvent;
 import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
@@ -76,21 +83,56 @@ public class EntityListener {
     }
 
     /**
-     * Saves event records when an entity dies.
+     * Saves event records when an entity dies. And also breaking of armor stands, paintings and item frames
      *
-     * @param event DestructEntityEvent.Death
+     * @param event DestructEntityEvent
      */
     @Listener(order = Order.POST)
-    public void onDestructEntityDeath(DestructEntityEvent.Death event, @Getter("getTargetEntity") Entity entity) {
-        if (!Prism.getInstance().getConfig().getEventCategory().isEntityDeath()) {
+    public void onDestructEntityDeath(DestructEntityEvent event, @Getter("getTargetEntity") Entity entity) {
+        if (Prism.getInstance().getConfig().getEventCategory().isBlockBreak()) {
+            if (entity instanceof Hanging || entity instanceof ArmorStand) {
+                final Cause cause = event.getCause();
+                final String targetFinal = entity.getType().getId().replace("_", " ");
+                PrismRecord.create()
+                        .source(cause)
+                        .target(targetFinal)
+                        .location(entity.getLocation())
+                        .event(PrismEvents.BLOCK_BREAK).
+                        buildAndSave();
+                return;
+            }
+        }
+        if (!Prism.getInstance().getConfig().getEventCategory().isEntityDeath() || !(event instanceof DestructEntityEvent.Death)) {
             return;
         }
-
         PrismRecord.create()
                 .source(event.getCause())
                 .event(PrismEvents.ENTITY_DEATH)
                 .entity(event.getTargetEntity())
                 .buildAndSave();
+    }
+
+    /**
+     * Saves event records when an is spawned (Used for tracking when item frames, armor stands and paintings are placed
+     *
+     * @param event SpawnEntityEvent
+     */
+    @Listener
+    public void onEntitySpawn(SpawnEntityEvent event) {
+        if (Prism.getInstance().getConfig().getEventCategory().isBlockPlace()) {
+            event.filterEntities(entity -> {
+                if (entity instanceof Hanging || entity instanceof ArmorStand) {
+                    final String targetFinal = entity.getType().getId().replace("_", " ");
+                    PrismRecord.create()
+                            .source(cause)
+                            .target(targetFinal)
+                            .location(entity.getLocation())
+                            .event(PrismEvents.BLOCK_PLACE).
+                            buildAndSave();
+                }
+                return true;
+            });
+        }
     }
 
     /**
